80d0d97bdb51aeb8a9be9d961f9ddf3d2bea77a6
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-07-19 23:45:01 -0300

    "Estructura de Carpetas"

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+const PORT = 4200;
+
+app.listen(PORT, () => {
+    console.log('El servidor está funcionando en el puerto '+PORT);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
+
+app.get('/productCart/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/productCart.html'));
+});
