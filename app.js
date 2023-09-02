bb00d00eba05c493fe3b8892ab02ff490b0b331e
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-09-01 23:28:15 -0300

    Arreglo en las rutas y uso de Morgan

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 //const formAdminRouter = require('./routers/formsAdministrador');
 const express = require('express');
+const logger = require('morgan');
+const methodOverride = require('method-override');
 const app = express();
 const homeRouter = require('./routers/home');
 const productRouter = require('./routers/product');
 const usersRouter = require('./routers/users');
-const methodOverride = require('method-override');
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json())
+app.use(logger('dev'));
 app.use(methodOverride('_method'));
 app.use(express.static('./public'));
 app.set('view engine','ejs');
@@ -18,29 +20,22 @@ app.listen(port, () => {
     console.log('El servidor está funcionando en el puerto '+ port);
 });
 
-
 app.use('/',homeRouter);
 
-<<<<<<< HEAD
-app.use('/productCart',productRouter);
-
-app.use('/',productRouter);
-
-app.use('/formularioAdministrador',formAdminRouter);
-=======
 app.use('/products',productRouter);
->>>>>>> bbcd432748900c22fd002bbb4ec39c1307185232
 
 app.use('/login', usersRouter);
 
 app.use('/', usersRouter);
 
-<<<<<<< HEAD
-=======
 //app.use('/productCart',productRouter);
 
 //app.use('/formularioAdministrador',formAdminRouter);
 
->>>>>>> bbcd432748900c22fd002bbb4ec39c1307185232
+/* app.use('/productCart',productRouter);
+
+app.use('/',productRouter);
+
+app.use('/formularioAdministrador',formAdminRouter); */
 
 
