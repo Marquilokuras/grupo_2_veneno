c7ba90a4fb56110079d8b8b2aa3a4129eadd6f01
Darkos0912 <villanuevadarkos@gmail.com>
2023-10-11 21:55:22 -0300

    Implementación Sequelize y Modelos

---
+++ src/database/models/Product.js
@@ -0,0 +1,56 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = "Product";
+    const cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING(255)
+        },
+        price: {
+            type: dataTypes.DECIMAL(3,1)
+        },
+        description: {
+            type: dataTypes.STRING(255)
+        },
+        image: {
+            type: dataTypes.STRING(255)
+        },
+        availability: {
+            type: dataTypes.BOOLEAN
+        },
+        amount: {
+            type: dataTypes.INTEGER
+        },
+        cartSale: {
+            type: dataTypes.BOOLEAN
+        },
+        category: {
+            type: dataTypes.STRING(255)
+        },
+        gender: {
+            type: dataTypes.STRING(30)
+        },
+        discount: {
+            type: dataTypes.INTEGER
+        }
+    };
+    const config = {
+        tableName: "products",
+        timestamps: false
+    };
+
+    const Product = sequelize.define(alias,cols,config);
+    Product.associate = (models) => {
+        Product.belongsToMany(models.User, {
+            as: "users",
+            through: "users_products",
+            foreignKey: "product_id",
+            otherKey: "user_id",
+            timestamps: false
+        })
+    }
+    return Product;
+}
