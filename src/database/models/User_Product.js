602051866b2aaa8ce57db5a87969b58148e22865
Darkos0912 <villanuevadarkos@gmail.com>
2023-10-14 20:31:52 -0300

    Sequelize terminado

---
+++ src/database/models/User_Product.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = "User_Product";
+    const cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: dataTypes.INTEGER,
+            references: {
+                model: User,
+                key: id
+            }
+        },
+        product_id: {
+            type: dataTypes.INTEGER,
+            references: {
+                model: Product,
+                key: id
+            }
+        }
+    };
+    const config = {
+        tableName: "users_products",
+        timestamps: false
+    }
+
+    const UserProduct = sequelize.define(alias,cols,config);
+
+    //Faltan las asociaciones
+    return UserProduct;
+}
