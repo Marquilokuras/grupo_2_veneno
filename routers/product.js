d11db232ee8fa4ddff4f7c9d35cfa47a50794093
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-10-17 02:18:52 -0300

    CREE LA BUSQUEDA POR TIPO DE PRENDA Y GENERO

--- routers/product.js
+++ routers/product.js
@@ -21,6 +21,13 @@ router.get('/productCart', productController.renderProductCart);
 router.post('/productCart/:id', productController.processProductCart); 
 router.delete('/productCart/delete/:id',upload.single("imagen"), productController.destroy); 
 
+router.get('/girl',productController.girl);
+router.get('/man',productController.man);
+router.get('/remeras',productController.remeras);
+router.get('/abrigos',productController.abrigos);
+router.get('/pantalones',productController.pantalones);
+router.get('/accesorios',productController.accesorios);
+
 router.get('/', productController.products);                                          //1_ /products (GET) Listado de productos
 router.get('/create',productController.create)                                       //2_ /products/create (GET) Formulario de creacion de productos
 router.get('/:id', productController.detail);                                       //3_ /products/:id (GET) Detalle de un producto particular
